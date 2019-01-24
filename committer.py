344d110d631035c7f11cf78a6187e98db9d2277f
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2019-01-24 09:54:25 +0545

    Refactor and format code.

--- committer.py
+++ committer.py
@@ -1,12 +1,12 @@
 class Committer(object):
     name = ""
     email = ""
-    count = 0
+    commit_count = 0
 
     def __init__(self, name, email):
         self.name = name
         self.email = email
-        self.count = 1
+        self.commit_count = 1
 
-    def add_commit(self):
-        self.count += 1
+    def increment_commit_count(self):
+        self.commit_count += 1
--- main.py
+++ main.py
@@ -2,33 +2,56 @@ from git import Repo
 from committer import Committer
 import sys
 
+repo = None
+commits = []
+committer_list = []
+commits_in_span = [0, 0, 0, 0]
+commits_count = 0
 # git_dir = "/home/dhirajsharma/Drive/Arson/Projects/Python/GitAnalytics/"
 git_dir = "/home/dhirajsharma/Drive/SmartMobe/Projects/Android/4service-android/"
 
-try:
-    repo = Repo(git_dir)
-    commits = list(repo.iter_commits())
-except:
-    print("Not valid git repo or no commits available.")
-    sys.exit(1)
 
-committer_list = []
-commits_count = len(commits)
-print("Total commits : " + str(commits_count))
-for commit in commits:
-    index = next((i for i, item in enumerate(committer_list) if item.email == commit.author.email), -1)
-    if index == -1:
-        committer_list.append(Committer(commit.author.name, commit.author.email))
-    else:
-        committer_list[index].add_commit()
+def get_committer_index(commit):
+    return next((i for i, item in enumerate(committer_list) if item.email == commit.author.email), -1)
 
-committer_list.sort(key=lambda x: x.count, reverse=True)
-for committer in committer_list:
-    print(
-        str(committer.count) +
-        " commits (" +
-        str(round(committer.count / commits_count * 100, 1)) +
-        "%) from " +
-        committer.name +
-        "(" + committer.email + ")"
-    )
+
+def main():
+    global repo, commits, commits_count
+
+    try:
+        repo = Repo(git_dir)
+        commits = list(repo.iter_commits())
+    except:
+        print("Not valid git repo or no commits available.")
+        sys.exit(1)
+
+    commits_count = len(commits)
+    print("Total commits : " + str(commits_count))
+    filter_commits_for_committer()
+
+
+def filter_commits_for_committer():
+    for commit in commits:
+        index = get_committer_index(commit)
+        if index == -1:
+            committer_list.append(Committer(commit.author.name, commit.author.email))
+        else:
+            committer_list[index].increment_commit_count()
+
+    committer_list.sort(key=lambda x: x.commit_count, reverse=True)
+    log_committer_stats()
+
+
+def log_committer_stats():
+    for committer in committer_list:
+        print(
+            str(committer.commit_count) +
+            " commits (" +
+            str(round(committer.commit_count / commits_count * 100, 1)) +
+            "%) from " +
+            committer.name +
+            "(" + committer.email + ")"
+        )
+
+
+main()
