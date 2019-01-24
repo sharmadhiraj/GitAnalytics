83db00fdf644d7ad127c5244743c8b359e51266b
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2019-01-24 10:34:02 +0545

    Add stats for commit by time.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from git import Repo
 from committer import Committer
 import sys
@@ -5,17 +7,27 @@ import sys
 repo = None
 commits = []
 committer_list = []
-commits_in_span = [0, 0, 0, 0]
+commits_in_span = [0, 0, 0, 0, 0]
 commits_count = 0
-# git_dir = "/home/dhirajsharma/Drive/Arson/Projects/Python/GitAnalytics/"
-git_dir = "/home/dhirajsharma/Drive/SmartMobe/Projects/Android/4service-android/"
+current_time = int(time.time())
+git_dir = "/home/dhirajsharma/Drive/Arson/Projects/Python/GitAnalytics/"
+# git_dir = "/home/dhirajsharma/Drive/SmartMobe/Projects/Android/4service-android/"
 
 
 def get_committer_index(commit):
     return next((i for i, item in enumerate(committer_list) if item.email == commit.author.email), -1)
 
 
-def main():
+def format_commit_count(count):
+    return str(count) + " commits (" + str(
+        round(count / commits_count * 100, 1)) + "%)"
+
+
+def divider():
+    print("=========================================================================================")
+
+
+def set_up_repo():
     global repo, commits, commits_count
 
     try:
@@ -27,7 +39,6 @@ def main():
 
     commits_count = len(commits)
     print("Total commits : " + str(commits_count))
-    filter_commits_for_committer()
 
 
 def filter_commits_for_committer():
@@ -39,19 +50,49 @@ def filter_commits_for_committer():
             committer_list[index].increment_commit_count()
 
     committer_list.sort(key=lambda x: x.commit_count, reverse=True)
-    log_committer_stats()
 
 
 def log_committer_stats():
+    print("Commits by author")
     for committer in committer_list:
         print(
-            str(committer.commit_count) +
-            " commits (" +
-            str(round(committer.commit_count / commits_count * 100, 1)) +
-            "%) from " +
+            format_commit_count(committer.commit_count) +
+            " from " +
             committer.name +
             "(" + committer.email + ")"
         )
 
 
-main()
+def filter_commits_for_spans():
+    for commit in commits:
+        commit_ago = current_time - commit.authored_date
+        if commit_ago < 3600:
+            commits_in_span[0] += 1
+        if commit_ago < 86400:
+            commits_in_span[1] += 1
+        if commit_ago < 604800:
+            commits_in_span[2] += 1
+        if commit_ago < 2592000:
+            commits_in_span[3] += 1
+        if commit_ago < 31104000:
+            commits_in_span[4] += 1
+
+
+def log_spans_stats():
+    print("Commits by time")
+    print(format_commit_count(commits_in_span[0]) + " in last one hour")
+    print(format_commit_count(commits_in_span[1]) + " in last one day")
+    print(format_commit_count(commits_in_span[2]) + " in last one week")
+    print(format_commit_count(commits_in_span[3]) + " in last one month")
+    print(format_commit_count(commits_in_span[4]) + " in last one year")
+
+
+divider()
+set_up_repo()
+divider()
+filter_commits_for_committer()
+log_committer_stats()
+divider()
+filter_commits_for_spans()
+log_spans_stats()
+divider()
